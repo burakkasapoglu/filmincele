8f8dc7bb2830f59154ef290a77ec75327efbdf83
Burak Kasapoglu <brkkasapoglu@gmail.com>
2026-07-27 18:52:41 +0300

    AI blog: CSRF exception eklendi

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,6 +22,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
         $middleware->validateCsrfTokens(except: [
             'admin/blog/image-upload',
+            'admin/blog/ai-generate',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
