5cceff96b78ad02d077603b3de5dc25e7ddbf9df
Burak Kasapoglu <brkkasapoglu@gmail.com>
2026-07-28 12:28:54 +0300

    Yeni logo + favicon guncellendi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/png" href="{{ asset('favicon.ico') }}">
     <title>@yield('title', 'Filmincele') — Filmincele</title>
     <x-seo :title="$__env->yieldContent('title', 'Filmincele') . ' — Filmincele'" />
     <link rel="preconnect" href="https://fonts.bunny.net">
