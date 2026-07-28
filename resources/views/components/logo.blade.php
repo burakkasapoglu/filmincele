0e4a2c2c1e761ac599b31a7d7f36709b6810707a
Burak Kasapoglu <brkkasapoglu@gmail.com>
2026-07-28 12:40:04 +0300

    Logo orijinale donduruldu, CSS ile boyutlandiriliyor

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,3 +1,3 @@
-<a href="{{ url('/') }}" class="inline-flex items-center gap-2 group flex-shrink-0">
-    <img src="{{ asset('logo.png') }}" alt="Filmincele" class="h-9 w-auto">
+<a href="{{ url('/') }}" class="inline-flex items-center flex-shrink-0">
+    <img src="{{ asset('logo.png') }}" alt="Filmincele" class="h-7 sm:h-9 w-auto max-w-[160px] object-contain">
 </a>
