bac09b52bc3590572c92b5144f3316ddab982de3
Burak Kasapoglu <brkkasapoglu@gmail.com>
2026-07-28 12:41:19 +0300

    Logo fix: inline style ile kesin boyut

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,3 +1,3 @@
-<a href="{{ url('/') }}" class="inline-flex items-center flex-shrink-0">
-    <img src="{{ asset('logo.png') }}" alt="Filmincele" class="h-7 sm:h-9 w-auto max-w-[160px] object-contain">
+<a href="{{ url('/') }}" class="flex-shrink-0">
+    <img src="{{ asset('logo.png') }}" alt="Filmincele" style="height: 36px; width: auto; max-width: 150px;">
 </a>
