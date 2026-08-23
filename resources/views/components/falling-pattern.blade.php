645c9832339079713cda836b7e3d52b640fd7843
Burak Kasapoglu <brkkasapoglu@gmail.com>
2026-08-23 16:50:43 +0300

    Falling pattern gorunurlugu arttirildi: opaklik, hiz (60s), blur hafifletme

--- resources/views/components/falling-pattern.blade.php
+++ resources/views/components/falling-pattern.blade.php
@@ -1,13 +1,13 @@
 @props([
-    'speed' => '150s',
+    'speed' => '60s',
     'mask' => true,
 ])
 
 <div {{ $attributes->merge(['class' => 'pointer-events-none absolute inset-0 overflow-hidden']) }}>
     <div class="falling-pattern absolute inset-0 transition-opacity duration-500"
-         style="{{ $speed !== '150s' ? "animation-duration: {$speed};" : '' }}"></div>
+         style="animation-duration: {{ $speed }};"></div>
     @if($mask)
         <div class="falling-pattern-overlay absolute inset-0"></div>
-        <div class="absolute inset-0" style="background: radial-gradient(ellipse at 50% 40%, transparent 0%, rgb(3 7 18) 85%)"></div>
+        <div class="absolute inset-0" style="background: radial-gradient(ellipse at 50% 40%, transparent 0%, rgb(3 7 18 / 0.6) 90%)"></div>
     @endif
 </div>
